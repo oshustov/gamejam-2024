e53f75f712f82c531710a4c0d44ed115328f6b29
PLARIUM\o.shustov <o.shustov@plarium.com>
2024-06-06 10:18:37 +0300

    add game cubes

---
+++ gameJam2024/Assets/_Scripts/Managers/GameCubesSpawnManager.cs
@@ -0,0 +1,12 @@
+﻿using Assets._Scripts.Entities;
+
+namespace Assets._Scripts.Managers
+{
+  public class GameCubesSpawnManager : StaticInstance<GameCubesSpawnManager>
+  {
+    public void SetArray(GameCube[] cubes)
+    {
+
+    }
+  }
+}
