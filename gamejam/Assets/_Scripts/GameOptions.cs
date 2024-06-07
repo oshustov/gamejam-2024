27b52560bc3d11ef2e8f22349a850eed6fb7cb18
PLARIUM\i.prokopiev <i.prokopiev@plarium.com>
2024-06-07 09:57:11 +0300

    no message

--- gamejam/Assets/_Scripts/GameOptions.cs
+++ gamejam/Assets/_Scripts/GameOptions.cs
@@ -5,7 +5,7 @@
         public static readonly int InfluenceLevelNormal = 1;
         public static readonly int InfluenceLevelHard = 2;
         public static readonly int ClickCountForCurse = 10;
-        public static readonly int CurseFlipCount = 5;
+        public static readonly int CurseFlipCount = 7;
         public static readonly int RoundTimeInSeconds = 90;
         public static readonly int RoundTimeInSecondsHard = 180;
     }
